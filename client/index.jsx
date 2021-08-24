27120dffb2d9e987f084cfd981783bc9959a04ff
Keanu Hasty <keanu.hasty@gmail.com>
2021-08-24 17:23:54 -0600

    abstracted phaser files from main

--- client/index.jsx
+++ client/index.jsx
@@ -2,37 +2,16 @@ import Phaser from 'phaser';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import logoImg from './assets/logo.png';
-
-class MyGame extends Phaser.Scene {
-  constructor() {
-    super();
-  }
-
-  preload() {
-    this.load.image('logo', logoImg);
-  }
-
-  create() {
-    const logo = this.add.image(400, 150, 'logo');
-
-    this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: 'Power2',
-      yoyo: true,
-      loop: -1,
-    });
-  }
-}
+import MainMenu from './phaser/SceneMainMenu';
+import Main from './phaser/SceneMain';
+import GameOver from './phaser/SceneGameOver';
 
 const config = {
   type: Phaser.AUTO,
   parent: 'phaser',
   width: 800,
   height: 600,
-  scene: MyGame,
+  scene: [MainMenu, Main, GameOver],
 };
 
 const game = new Phaser.Game(config);
---
+++ client/phaser/SceneMainMenu.js
@@ -0,0 +1,27 @@
+import Phaser from 'phaser';
+import logoImg from '../assets/logo.png';
+
+class MainMenu extends Phaser.Scene {
+  constructor() {
+    super({ key: 'MainMenu' });
+  }
+
+  preload() {
+    this.load.image('logo', logoImg);
+  }
+
+  create() {
+    const logo = this.add.image(400, 150, 'logo');
+
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: 'Power2',
+      yoyo: true,
+      loop: -1,
+    });
+  }
+}
+
+export default MainMenu;
